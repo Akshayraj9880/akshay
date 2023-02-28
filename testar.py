eb1b676edfdb33c039bdfbcb7b3fc68524db1c9b
Akshayraj9880 <126572009+Akshayraj9880@users.noreply.github.com>
2023-02-28 18:54:15 +0530

    Add files via upload

---
+++ testar.py
@@ -0,0 +1,11 @@
+x=int(input("Enter your mark"))
+if x>24:
+    print("You have passed the exam")
+    if x>90:
+        print("You have A++")
+    else:
+         print("you don't have an A++")
+       
+else:
+    print("you have failed the exam")
+
