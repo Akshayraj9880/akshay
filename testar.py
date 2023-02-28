3fb13976294ff65b14c972817a1a9aeef2ae88ee
Akshayraj9880 <126572009+Akshayraj9880@users.noreply.github.com>
2023-02-28 19:08:01 +0530

    Add files via upload

--- testar.py
+++ testar.py
@@ -3,8 +3,14 @@ if x>24:
     print("You have passed the exam")
     if x>90:
         print("You have A++")
-    else:
-         print("you don't have an A++")
+    elif x>80 and x<90:
+        print("you have an A")
+    elif x>70 and x<80:
+        print ("you have a B")
+    elif x>60 and x<70:
+        print ("you have c")
+    elif x>50 and x<60:
+        print ("you have d")
        
 else:
     print("you have failed the exam")
